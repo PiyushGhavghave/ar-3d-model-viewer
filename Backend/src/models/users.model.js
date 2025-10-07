ae8f3f8943c4f964862907f80eb149c7e779765b
PiyushGhavghave <piyushghavghave@gmail.com>
2025-10-07 19:27:46 +0530

    Intial setup for admin view

--- Backend/src/models/users.model.js
+++ Backend/src/models/users.model.js
@@ -23,6 +23,15 @@ const userSchema = new mongoose.Schema({
       required: [true, 'Password is required'],
       minlength: [6, 'Password must be at least 6 characters']
     },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    isDisabled: {
+        type: Boolean,
+        default: false
+    },
     profilePicture: {
         type: String,
         default: ''
