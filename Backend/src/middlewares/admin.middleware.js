b6a7fd6eb9a1b98eafaaa3240e49f8309ee6b8f7
PiyushGhavghave <piyushghavghave@gmail.com>
2025-10-07 20:14:19 +0530

    Backend: Created admin middleware

---
+++ Backend/src/middlewares/admin.middleware.js
@@ -0,0 +1,11 @@
+import { apiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const verifyAdmin = asyncHandler(async (req, _, next) => {
+    if (req.user?.role !== 'admin') {
+        throw new apiError(403, "Forbidden: You are not authorized to perform this action.");
+    }
+    next();
+});
+
+export default verifyAdmin;
