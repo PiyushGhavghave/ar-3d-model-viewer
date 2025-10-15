d39c8ac8c897ed184dbc732cc7bd45a9a162f5f2
PiyushGhavghave <piyushghavghave@gmail.com>
2025-10-16 03:42:16 +0530

    Backend: Rename admin middleware

--- Backend/src/middlewares/admin.middleware.js
+++ Backend/src/middlewares/admin.middleware.js
@@ -1,11 +1,11 @@
 import { apiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const verifyAdmin = asyncHandler(async (req, _, next) => {
+const verifyAdminOrEditor = asyncHandler(async (req, _, next) => {
     if (req.user?.role !== 'admin' && req.user?.role !== 'editor') {
         throw new apiError(403, "Forbidden: You are not authorized to perform this action.");
     }
     next();
 });
 
-export default verifyAdmin;
+export default verifyAdminOrEditor;
--- Backend/src/routes/notification.route.js
+++ Backend/src/routes/notification.route.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { streamNotifications, getNotifications, markAllAsRead } from "../controllers/notification.controller.js";
 import verifyJWT from "../middlewares/auth.middleware.js";
-import verifyAdmin from "../middlewares/admin.middleware.js";
+import verifyAdminOrEditor from "../middlewares/admin.middleware.js";
 
 const router = Router();
 
-router.use(verifyJWT, verifyAdmin);
+router.use(verifyJWT, verifyAdminOrEditor);
 
 router.get("/stream", streamNotifications);
 router.get("/", getNotifications);
